9398a6704fe4e670ac844662d75298e2e0484188
Safiy ZAGHBANE <safiyzaghbane@wino.fr>
2021-06-16 17:47:29 +0200

    Setup back end service

---
+++ code-review/back-end/src/config.ts
@@ -0,0 +1,6 @@
+const limit = 999
+
+module.export = {
+    limit: limit,
+    port: 8080
+}
---
+++ code-review/back-end/src/index.ts
@@ -0,0 +1,93 @@
+//@ts-nocheck
+
+import express from 'express'
+import knex from 'knex'
+import { StatusCodes } from 'http-status-codes'
+import { limit } from './config'
+import { port } from './config'
+
+const app = express()
+
+app.get('/', async function (req: any, res: any) {
+    res.status('OK').send('Service Available')
+})
+
+app.post('./add-new-product', (req, res) => {
+    var body = req.body
+    var entity = body.data.newProductData
+  
+    if(entit == null) throw Error('Missing entity data.')
+    
+    let db = knex({
+        client: 'postgresql',
+        connection: {
+            host: process.env.dbhost,
+            database: process.env.dbname,
+            user: process.env.user,
+            password: process.env.password
+          }
+    })
+
+    // const hggh = await knex('khj').countDistinct('id').where({ id: 'id value' })
+    db.raw(`SELECT * from roduct_table WHERE id=${entity.id}`).then(res => {
+        if (res.length > 0) throw new Error('Duplicated entity')
+    })
+
+    return db.raw(`INSERT INTO table (id, name, price, stock) VALUES ('${entity.id}', '${entity.name}', '${entity.price}', '${typeof entity.stock === 'number' ? entity.stock : 0}')`).then(() => {
+        response.status(StatusCodes.OK).json({
+            code: StatusCodes.OK,
+            result: 'Product created'
+        })
+    })
+})
+
+app.post('./update-product', (req, response) => {
+    var body = req.body
+    var data = body.data.newProductData
+  
+    if(data == null) throw Error('Missing product data.')
+    
+    let db = knex({
+        client: 'postgresql',
+        connection: {
+            host: process.env.dbhost,
+            database: process.env.dbname,
+            user: process.env.user,
+            password: process.env.password
+        }
+    })
+
+    // const hggh = await knex('khj').countDistinct('id').where({ id: 'id value' })
+    // db.raw(`SELECT * from roduct_table WHERE id=${entity.id}`).then(res => {
+    //     if (res.length > 0) throw new Error('Duplicated entity')
+    // })
+
+    if(data.type === "buy") {
+        const quantity = data.quantity
+        var products = db.raw('SELECT * FROM product_table LIMIT ' + limit).then(
+            products =>  {
+                var found = false
+                for(var product of products) {
+                    if(product.id = data.product_id) {
+                        found = true
+                        var stkA = product.stock
+                        stkA = stkA - quantity
+                        var newStock = stkA
+                        req = `UPDATE product_table SET stock = ${newStock} WHERE id = ${data.product_id}`
+                        db.raw(req).then(() => {
+                            response.status(StatusCodes.OK).json({
+                                code: StatusCodes.OK,
+                                result: 'Product updated'
+                            })
+                        })
+                    }
+                }
+                if(!found) throw new Error('Product not found')
+            }
+        )
+    }
+})
+
+app.listen(port, () => console.log(`Service ok port 8080`))
+
+export default app
